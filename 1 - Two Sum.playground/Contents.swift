dc37e4210756e557376c1a8ee51170c10680b2f6
yusufcanbircan <yusufcannnz@hotmail.com>
2022-08-17 22:50:57 +0300

    .

--- 1 - Two Sum.playground/Contents.swift
+++ 1 - Two Sum.playground/Contents.swift
@@ -1,17 +1,1 @@
-import UIKit
-import Foundation
 
-class Solution {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var result = [Int]()
-            
-        for i in 0..<nums.count-1{
-            for j in i+1..<nums.count{
-                if nums[i] + nums[j] == target{
-                    result = [i, j]
-                }
-            }
-        }
-        return result
-    }
-}
